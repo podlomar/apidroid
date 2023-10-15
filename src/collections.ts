e431930eed1b81f8ee2972b3d4a8f87c1237d281
Martin Podloucký <podlouckymartin@gmail.com>
2023-10-15 12:37:02 +0200

    All the basic CRUD operations

--- src/collections.ts
+++ src/collections.ts
@@ -8,10 +8,11 @@ export interface Collection {
   items: any[],
 }
 
+export type WithId<T> = T & { id: number };
+
 export const loadCollection = async (
   fsPath: string,
 ): Promise<Result<Collection, 'error'>> => {
-  console.log(fsPath);
   const filePath = existsSync(fsPath)
     ? path.resolve(fsPath, 'collection.json')
     : fsPath + '.json';
@@ -32,13 +33,37 @@ export const loadCollection = async (
   return Result.success({ filePath, lastId, items });
 };
 
-export const addToCollection = async (
-  collection: Collection, item: any,
-): Promise<Result<'ok', 'error'>> => {
+export const addToCollection = async <T>(
+  collection: Collection, item: T,
+): Promise<Result<WithId<T>, 'error'>> => {
   const newItem = { id: collection.lastId + 1, ...item };
   await fs.writeFile(
     collection.filePath,
     JSON.stringify([...collection.items, newItem], null, 2),
+  );
+
+  return Result.success(newItem);
+}
+
+export const updateInCollection = async <T>(
+  collection: Collection, item: WithId<T>,
+): Promise<Result<WithId<T>, 'error'>> => {
+  const items = collection.items.map((i) => i.id === item.id ? item : i);
+  await fs.writeFile(
+    collection.filePath,
+    JSON.stringify(items, null, 2),
+  );
+
+  return Result.success(item);
+}
+
+export const removeFromCollection = async <T>(
+  collection: Collection, id: number,
+): Promise<Result<'ok', 'error'>> => {
+  const items = collection.items.filter((item) => item.id !== id);
+  await fs.writeFile(
+    collection.filePath,
+    JSON.stringify(items, null, 2),
   );
 
   return Result.success('ok');
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import express from 'express';
-import { loadCollection } from './collections.js';
+import { addToCollection, loadCollection, removeFromCollection, updateInCollection } from './collections.js';
 import { payload } from './payload.js';
 
 const baseDir = process.argv[2] ?? process.cwd();
@@ -8,12 +8,13 @@ const baseDir = process.argv[2] ?? process.cwd();
 const app = express();
 
 app.use('/assets', express.static(path.resolve(baseDir, 'assets')));
+app.use(express.json());
 
-const pathSegment = '[a-z][a-z_\-]*';
-const colletionPath = `^/api(/${pathSegment})+`;
+const pathSegment = '[a-z][a-z_-]*';
+const collectionPath = `^/api(/${pathSegment})+`;
 const idPattern = '[0-9]+';
 
-app.get(new RegExp(`${colletionPath}$`), async (req, res) => {
+app.get(new RegExp(`${collectionPath}$`), async (req, res) => {
   const fsPath = path.resolve(baseDir, req.path.slice(1));
   const collectionResult = await loadCollection(fsPath);
   if (collectionResult.isFail()) {
@@ -25,7 +26,7 @@ app.get(new RegExp(`${colletionPath}$`), async (req, res) => {
   res.json(payload('ok', collection.items));
 });
 
-app.get(new RegExp(`${colletionPath}/${idPattern}$`), async (req, res) => {
+app.get(new RegExp(`${collectionPath}/${idPattern}$`), async (req, res) => {
   const fsPath = path.resolve(
     baseDir, req.path.slice(1, req.path.lastIndexOf('/')),
   );
@@ -48,6 +49,79 @@ app.get(new RegExp(`${colletionPath}/${idPattern}$`), async (req, res) => {
   }
 
   res.json(payload('ok', item));
+});
+
+app.post(new RegExp(`${collectionPath}$`), async (req, res) => {
+  const fsPath = path.resolve(baseDir, req.path.slice(1));
+  const collectionResult = await loadCollection(fsPath);
+  if (collectionResult.isFail()) {
+    res.status(404).send('Not found');
+    return;
+  };
+
+  const collection = collectionResult.get();
+  const body = req.body;
+
+  const newItem = await addToCollection(collection, body);
+  if (newItem.isFail()) {
+    res.status(500).json(payload('server-error', [{
+      code: 'error',
+      message: 'Error while saving item',
+    }]));
+    return;
+  }
+
+  res.status(201).json(payload('ok', { id: newItem.get().id }));
+});
+
+app.put(new RegExp(`${collectionPath}/${idPattern}$`), async (req, res) => {
+  const fsPath = path.resolve(
+    baseDir, req.path.slice(1, req.path.lastIndexOf('/')),
+  );
+  const id = Number(req.path.slice(req.path.lastIndexOf('/') + 1))
+
+  const collectionResult = await loadCollection(fsPath);
+  if (collectionResult.isFail()) {
+    res.status(404).send('Not found');
+    return;
+  };
+
+  const collection = collectionResult.get();
+  const result = await updateInCollection(collection, { ...req.body, id });
+  if (result.isFail()) {
+    res.status(500).json(payload('server-error', [{
+      code: 'error',
+      message: 'Error while saving item',
+    }]));
+    return;
+  }
+
+  res.json(payload('ok', result.get()));
+});
+
+app.delete(new RegExp(`${collectionPath}/${idPattern}$`), async (req, res) => {
+  const fsPath = path.resolve(
+    baseDir, req.path.slice(1, req.path.lastIndexOf('/')),
+  );
+  const id = Number(req.path.slice(req.path.lastIndexOf('/') + 1))
+
+  const collectionResult = await loadCollection(fsPath);
+  if (collectionResult.isFail()) {
+    res.status(404).send('Not found');
+    return;
+  };
+
+  const collection = collectionResult.get();
+  const result = await removeFromCollection(collection, id);
+  if (result.isFail()) {
+    res.status(500).json(payload('server-error', [{
+      code: 'error',
+      message: 'Error while saving item',
+    }]));
+    return;
+  }
+
+  res.json(payload('ok', `Item with id ${id} was deleted`));
 });
 
 app.listen(4000, () => {
