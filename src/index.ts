7116c487a055323c03d1ecd744765e539fcf391e
Martin Podloucký <podlouckymartin@gmail.com>
2023-10-15 19:01:15 +0200

    Configurable port

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { createServer } from './server.js';
 
 const baseDir = process.argv[2];
+const port = process.argv[3] ?? 4000;
 
 const server = createServer({
   baseDir, 
   maxItems: 1000,
 });
 
-server.listen(4000, () => {
-  console.log('Server is listening on port 4000');
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}...`);
 });
