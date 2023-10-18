c8f95f4585cb2c87485256432648c25744402885
Martin Podloucký <podlouckymartin@gmail.com>
2023-10-19 00:02:35 +0200

    Boxed messages

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@
 import { createServer } from './server.js';
 import parser from 'yargs-parser';
 import detect from 'detect-port';
+import boxen from 'boxen';
 
 const argv = parser(process.argv.slice(2));
 const port = argv.port || 4000;
@@ -14,10 +15,19 @@ const server = createServer({
 
 const freePort = await detect(port);
 
-if (freePort !== port) {
-  console.log(`WARNING: Port ${port} is already in use, using port ${freePort} instead`);
-}
-
 server.listen(freePort, () => {
-  console.log(`Server is running on http://localhost:${freePort}`);
+  console.log(
+    boxen(
+      (freePort !== port
+        ? `WARNING: Port ${port} is already in use, using port ${freePort} instead\n\n`
+        : ''
+      ) +
+      `Server is running on http://localhost:${freePort}`,
+      {
+        padding: 1,
+        margin: 1,
+        borderColor: 'green',
+      },
+    ),
+  );
 });
