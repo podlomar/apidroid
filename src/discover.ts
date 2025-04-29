55975a8fb5e326c67b344efa2383770f1f0114a3
Martin Podloucký <podlouckymartin@gmail.com>
2025-04-29 09:30:59 +0200

    Normalize path separators in discover function to work on both Linux and Windows

--- src/discover.ts
+++ src/discover.ts
@@ -20,8 +20,7 @@ export const discover = (baseDir: string): CollectionEntry[] => {
 
   const result: CollectionEntry[] = [];
   for (const entry of entries) {
-    const relativePath = path.relative(baseDir, entry.path);
-
+    const relativePath = path.relative(baseDir, entry.path).replace(/\\/g, '/');
     if (fileCollectionRegex.test(relativePath)) {
       const urlPath = relativePath.slice(0, relativePath.lastIndexOf('.'));
       result.push({
